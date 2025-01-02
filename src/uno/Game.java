8252368b8d91510c9fa641e0fe9940e35ede7879
noufel-bendar <bendarnoufel@gmail.com>
2025-01-02 11:19:54 +0100

    add method to class player

--- src/uno/Game.java
+++ src/uno/Game.java
@@ -1,9 +1,3 @@
 package uno;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Game {
-    public static void main(String[] args) {
-
-    }
 }
--- src/uno/Player.java
+++ src/uno/Player.java
@@ -1,67 +1,90 @@
 package uno;
-
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Player {
-    private String name;
-    private ArrayList<Card> hand;
-    private boolean isTurn;
+private String name;
+private ArrayList<Card> hand;
+private boolean isTurn;
 
-
-    // Constructor for initializing the player with a name
-    public Player(String name) {
-        this.name = name;
-        this.hand = new ArrayList<>();
-        this.isTurn = false;
+public Player (String name){
+    this.name= name;
+    this.hand= new ArrayList<Card>(); // it is empty arry
+    this.isTurn=false;
+}
+public String getName(){
+    return name;
+}
+public ArrayList<Card> getHand (){
+    return hand;
+    }
+public boolean isTurn(){
+    return isTurn;
+}
+public void setTurn (boolean isTurn){
+    this.isTurn=isTurn;
+}
+public boolean isWinner (){
+    return hand.isEmpty();
+}
+public void removeCard(Card card){
+    hand.remove(card);
+}
+public void drawCard(Card card){
+    hand.add(card);
+}
+public int getCardCount(){
+    return hand.size();
+}
+public boolean hasPlayableCard(Card card){// if the player has a playable card
+    Rules rules = new Rules();
+    for(int i=0;i<hand.size();i++){
+        if(rules.isValidMove(hand.get(i), card)) {
+            return true;
+        }
+    }
+    return false;
+}
+public ArrayList<Card> getPlayableCards(Card card){ // creat a list of playable cards
+    ArrayList<Card> playable =new ArrayList<Card>();
+    Rules rule =new Rules();
+    for(int i=0;i<hand.size();i++){
+        if(rules.isValidMove(hand.get(i), card)){// if you can play the card then add it to the array of  playable cards
+            playable.add(hand.get(i));
+        }
+    }
+    return playable;
+}
+public Card selectPlayableCard(Card card){//pick a card from the available cards
+    ArrayList<Card> playable =getPlayableCards(card);
+    if(playable.isEmpty()==false){
+        System.out.println(name +"select a card to play:");
+        for(int i=0; i<playable.size(); i++){
+            System.out.println((i+1)+":"+playable.get(i));
+        }
+        Scanner in =new Scanner(System.in);
+        int  s=in.nextInt()-1;
+        if( s>=0   &&   s<playable.size() ){
+            return playable.get(s);
+        }
+    }
+        return null;
     }
 
-    // Getter for the player's name
-    public String getName() {
-        return name;
-    }
-
-    // Getter for the player's hand
-    public ArrayList<Card> getHand() {
-        return hand;
-    }
-
-    // Setter to indicate if it is the player's turn
-    public void setTurn(boolean isTurn) {
-        this.isTurn = isTurn;
-    }
-
-    // Method to check if it is the player's turn
-    public boolean isTurn() {
-        return isTurn;
-    }
-
-    // Method to draw a card and add it to the player's hand
-    public void drawCard(Card card) {
-        hand.add(card);
-    }
-
-    // Method to remove a card from the player's hand
-    public void removeCard(Card card) {
-        hand.remove(card);
-    }
-
-
-
- // Method to check if the player has won
-    public boolean isWinner() {
-        return hand.isEmpty();
-    }
-
-
-    // Method to get the count of cards in the player's hand
-    public int getCardCount() {
-        return hand.size();
-    }
-
-    // Method to represent the player as a string
     @Override
     public String toString() {
-        return "Player: " + name + ", Cards: " + hand + ", Turn: " + isTurn;
+        return "player: " + name + " cards: " + hand + " turn: " + isTurn;
     }
-}
+    public void playCard(Card card) {
+    if (card!= null) {
+        removeCard(card);
+        System.out.println(name + " played: " + card);
+        } else {
+            System.out.println(name + "no valid card to play");
 
+    }
+    }
+
+
+}
