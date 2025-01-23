53454fc5492f349b4a1fbd5ce07b672efa1acf77
IFTENE Rayan <rayan.iftene5@gmail.com>
2025-01-23 17:13:00 +0100

    adding class Rules

---
+++ src/uno/Rules.java
@@ -0,0 +1,43 @@
+package uno;
+
+public class Rules {
+    public boolean isValidMove(Card card, Card topCard) {
+        if (card.getColor() == topCard.getColor()) {
+            return true;
+        }
+        if (card.getValue() == topCard.getValue()) {
+            return true;
+        }
+        if (card.getColor() == Card.Color.WILD) {
+            return true;
+        }
+        return false;
+    }
+    public void applyEffect(Card card, Game game) {
+        switch (card.getValue()) {
+            case DRAW_TWO:
+                enforceDrawCardsEffect(2, game);
+                break;
+            case WILD_DRAW_FOUR:
+                game.chooseNextColor();
+                enforceDrawCardsEffect(4, game);
+
+                break;
+            case REVERSE:
+                game.reverseDirection();
+                break;
+            case SKIP:
+                game.skipNextPlayer();
+                break;
+            default:
+                // Nothing
+                break;
+        }
+    }
+    private void enforceDrawCardsEffect(int count, Game game) {
+        Player nextPlayer = game.getNextPlayer();
+        for (int i = 0 ; i < count ; i++) {
+            nextPlayer.drawCard(game.drawCard());
+        }
+    }
+}
