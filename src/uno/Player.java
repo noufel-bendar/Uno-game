d517ce2092823eac3337d7c03b9b5a70ff5af9d7
noufel-bendar <bendarnoufel@gmail.com>
2025-01-23 10:45:39 +0100

    add  humanplayer class

--- src/uno/Player.java
+++ src/uno/Player.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class Player {
+public abstract class Player {
 private String name;
 private ArrayList<Card> hand;
 private boolean isTurn;
@@ -56,33 +56,11 @@ public ArrayList<Card> getPlayableCards(Card card){ // creat a list of playable 
     }
     return playable;
 }
-public Card selectPlayableCard(Card card){//pick a card from the available cards
-    ArrayList<Card> playable=getPlayableCards(card);
-    if(playable.isEmpty()==false){
-        System.out.println(name +"select a card to play:");
-        for(int i=0; i<playable.size(); i++){
-            System.out.println((i+1)+":"+playable.get(i));
-        }
-        Scanner in =new Scanner(System.in);
-        int  s=in.nextInt()-1;
-        if( s>=0     &&      s<playable.size() ){
-            return playable.get(s);
-        }
-    }
-        return null;
-    }
-
+public abstract Card selectPlayableCard(Card card);
     @Override
     public String toString() {
         return "player: " + name + " cards: " + hand + " turn: " + isTurn;
     }
-    public void playCard(Card card) {
-    if (card!= null) {
-        removeCard(card);
-        System.out.println(name + " played: " + card);
-        } else {
-            System.out.println(name + "no valid card to play");
-
-    }
+    public abstract void playCard(Card card);
     }
 }
